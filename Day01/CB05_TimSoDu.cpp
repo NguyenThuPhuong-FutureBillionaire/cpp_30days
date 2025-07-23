4911129df03dcbc944bd19034572e4ca10250c4f
CodeLikeTeu <thuphuong24092010@gmail.com>
2025-07-23 17:01:47 +0700

    Create CB05_TimSoDu.cpp

---
+++ Day01/CB05_TimSoDu.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // Khai báo và nhập a, b
+    int a, b;
+    cin >> a >> b;
+
+    // Tính và in ra dư a/b
+    cout << a%b;
+    return 0;
+}
