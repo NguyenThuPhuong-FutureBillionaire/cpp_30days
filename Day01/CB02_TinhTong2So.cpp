c5fd9c91fd1a1874bb4dc523472c60a1cdb89ab7
CodeLikeTeu <thuphuong24092010@gmail.com>
2025-07-23 16:38:06 +0700

    Create CB02_TinhTong2So.cpp

---
+++ Day01/CB02_TinhTong2So.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //khai bao a, b
+    int a, b;
+    // nhap a, b
+    cin >> a >> b;
+    // tính tổng a + b
+    int tong = a + b;
+    // in ra res
+    cout << tong << endl;
+    return 0;
+}
