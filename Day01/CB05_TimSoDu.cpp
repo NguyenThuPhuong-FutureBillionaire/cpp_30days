bf511f866316bab58cd8cee5e71d32ac8a019d9f
CodeLikeTeu <thuphuong24092010@gmail.com>
2025-07-23 17:03:08 +0700

    Update CB05_TimSoDu.cpp

--- Day01/CB05_TimSoDu.cpp
+++ Day01/CB05_TimSoDu.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
-    // Khai báo và nhập a, b
+    // Khai báo biến a, b
     int a, b;
+
+    // Nhập 2 số nguyên
     cin >> a >> b;
 
-    // Tính và in ra dư a/b
-    cout << a%b;
+    // Kiểm tra chia cho 0
+    if (b == 0)
+        cout << "ERROR";
+    else
+        cout << a % b;
+
     return 0;
 }
